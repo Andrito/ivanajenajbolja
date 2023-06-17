7f02ecb21c6dd47a2b2ee4b5f5d715dc39ae71a9
Andrej Dujovic <andrejdujovic@gmail.com>
2023-06-17 11:37:43 +0200

    Changed text

--- index.js
+++ index.js
@@ -33,7 +33,7 @@
 
             //do something later when date is reached
             if (distance < 0) {
-                document.getElementById("headline").innerText = "Srećan rođendan Ivanice!";
+                document.getElementById("headline").innerText = "Srećan rođendan Ivanice!!!";
                 document.getElementById("countdown").style.display = "none";
                 document.getElementById("content").style.display = "block";
                 clearInterval(x);
